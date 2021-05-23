336f1c5ab627332cd5c2851a87d8e8949d98862f
Saddd10 <73766214+Saddd10@users.noreply.github.com>
2021-05-23 16:50:54 +0700

    soal2b done

--- soal2/soal2b.c
+++ soal2/soal2b.c
@@ -3,61 +3,40 @@
 #include <pthread.h>
 #include <sys/ipc.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #define R 4
 #define C 6
 
-unsigned long long angka;
-int nilaibaru[4][6];
-pthread_t thread,thread2;
+typedef long long ll;
+int nilaibaru[4][6],beda,zero;
 
-unsigned long long factorial(unsigned long long a){
-    if(a==0 || a==1) return 1;
-    else return a*factorial(a-1);
+ll factorial(int n) {
+    if (n == 0) return 1;
+    return n*factorial(n-1);
 }
 
-void *hitungmatriks(void *arg)
-{
-	//template shared memory
-        key_t key = 1234;
-        int (*value)[10];
-        int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
-        value = shmat(shmid, 0, 0);
-        
-        printf("Input matriks baru: \n");
-    	for(int x=0;x<R;x++){
-			for(int y=0;y<C;y++){
-				scanf("%d", &nilaibaru[x][y]);
-			}
-		}
-
-    	printf("Hasil Faktorial Pertambahan Matriks tersebut adalah: \n");
-        for(int i=0;i<R;i++){
-		    for(int j=0;j<C;j++){
-				if(value[i][j] != 0 && nilaibaru[i][j] != 0){
-                	if(value[i][j] >= nilaibaru[i][j]){
-                    	angka = (factorial(value[i][j])/factorial(value[i][j] - nilaibaru[i][j]));
-                	}
-                	else if(value[i][j] < nilaibaru[i][j]){
-                    	angka = factorial(value[i][j]);
-                	}
-                	else if(value[i][j] == 0 && nilaibaru[i][j] == 0){
-                    	angka = 0;
-                	}
-			    	printf("%llu\t", angka); 
-				}
-				else printf("0\t");
-		    }
-		    printf("\n");
-	    }
-    pthread_exit(0);
+ll factorial2(int n){
+	if (n == beda) return 1;
+        return n*factorial2(n-1);
 }
+
+void *finalmatriks(void* argv){
+	ll n = *(ll*)argv;
+	if(zero){
+		printf("0 ");
+	}
+	else if(beda<1){
+		printf("%lld ", factorial(n));
+	} 
+	else printf("%lld ", factorial2(n));
+
 
 void main()
 {
     //Shared Memory
     key_t key = 1234;
-    int (*value)[10], nilaibaru[10];
+    int (*value)[10];
     int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
     value = shmat(shmid, 0, 0);
 
@@ -69,8 +48,33 @@ void main()
 	printf("\n");
 	}
 
-	printf("\n");
+	printf("Input matriks baru: \n");
+    for(int x=0;x<R;x++){
+		for(int y=0;y<C;y++){
+			scanf("%d", &nilaibaru[x][y]);
+		}
+	}
 
-	pthread_create(&thread, NULL, hitungmatriks, NULL);
-	pthread_join(thread,NULL);
+	printf("\n");
+	pthread_t thread_id[R*C];
+    int cnt=0;
+    for(int i = 0; i < R; i++){
+        for(int j = 0; j < C; j++){
+        	zero=0;
+            ll *val = malloc(sizeof(ll[4][6]));
+
+            *val = value[i][j];
+            beda = value[i][j] - nilaibaru[i][j];
+
+            if(value[i][j]==0 || nilaibaru[i][j]==0) zero=1;
+
+            pthread_create(&thread_id[cnt], NULL, &finalmatriks, val);
+            sleep(1);
+            cnt++;
+        }
+        printf("\n");
+    }
+    for (int i = 0; i<cnt; i++){
+		pthread_join(thread_id[i], NULL);
+	}
 }
